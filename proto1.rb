afcfe1dd5cfdb1d77b2a6a1b2da13e024890d884
Akama Hitoshi <akamahitoshi@gmail.com>
2015-08-31 15:57:56 +0900

    implemented Store, Bool, Set, Get, Equal (not tested yet)

--- proto1.rb
+++ proto1.rb
@@ -14,12 +14,22 @@ module Garbanzo
     attr_def = attrs.length > 0 ? "attr_accessor " + attr_list : ""
     arguments = attrs.join(', ')
     assignment = attrs.map {|x| "@" + x + " = " + x }.join('; ')
-    
+
+    hash_def = attrs.map {|x| x + ".hash" }.join(' ^ ')
+    eql_def  = attrs.map {|x| x + ".eql?(other)" }.join(' && ')
     str = <<"EOS"
 class #{classname} < #{opts[:extend] || "Object"}
   #{attr_def}
   def initialize(#{arguments})
     #{assignment}
+  end
+
+  def hash
+    #{hash_def}
+  end
+
+  def eql?(other)
+    #{eql_def}
   end
 end
 EOS
@@ -34,19 +44,16 @@ EOS
   module Repr
     Garbanzo.define_record_class(self, "Num", "num") # 言語の内部表現としての整数
     Garbanzo.define_record_class(self, "String", "value")  # 内部表現としての文字列
+    Garbanzo.define_record_class(self, "Bool", "value")  # 内部表現としての文字列
 
     Garbanzo.define_record_class(self, "Add", "left", "right") # 言語の内部表現としての足し算
     Garbanzo.define_record_class(self, "Mult", "left", "right") # 言語の内部表現としての掛け算
+    Garbanzo.define_record_class(self, "Equal", "left", "right") # 同じかどうかを判定
+    
     Garbanzo.define_record_class(self, "Print", "value") # print式を意味する内部表現
     Garbanzo.define_record_class(self, "Unit")  # いわゆるNOP
     Garbanzo.define_record_class(self, "Store", "table")  # データストアオブジェクト
 
-    # この辺はあったら便利だと思うので。
-    class Store
-      def []=(key, value);  table.[]=(key, value); end
-      def [](key);          table.[](key); end
-    end
-    
     Garbanzo.define_record_class(self, "Set", "object", "key", "value")  # データストアへの代入を表す
     Garbanzo.define_record_class(self, "Get", "object", "key")  # データストアからの読み出しを表す
 
@@ -55,18 +62,28 @@ EOS
     class Evaluator
       def evaluate(program)
         case program
-        when Num, Unit
+        when Num, Unit, Store, Bool
           program
         when Add
           Num.new(evaluate(program.left).num + evaluate(program.right).num)
         when Mult
           Num.new(evaluate(program.left).num * evaluate(program.right).num)
+        when Equal
+          Bool.new(program.left.eql?(program.right))
         when Print
           result = evaluate(program.value)
           p result
           result
+        when Set
+          obj, key, value = [program.object, program.key, program.value]
+
+          raise "SET: object is not a store #{obj}" if obj.is_a? Store
+          obj.table[key] = value
+        when Get
+          obj, key = [program.object, program.key]
+          obj.table[key]
         else
-          raise "argument is not a program"
+          raise "EVALUATE: argument is not a program"
         end
       end
     end

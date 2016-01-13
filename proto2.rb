908c4d2bd8bef9b8e536cbb6585a1294d43c57fe
Akama Hitoshi <akamahitoshi@gmail.com>
2016-01-13 15:20:45 +0900

    delete unused codes

--- proto2.rb
+++ proto2.rb
@@ -63,7 +63,6 @@ module Garbanzo
     end
 
     def install_whitespaces_rule(root)
-      #root['parser']['whitespaces'] = false.to_repr
       root['parser']['whitespaces'] =
         Repr::many(
           Repr::quote(
@@ -149,14 +148,6 @@ module Garbanzo
       root['parser'] = parser
 
       parser['sentence'] = Repr::choice(Repr::store({}))
-      # parser['sentence']['children']['homu'] = Repr::begin(
-      #   Repr::store({
-      #     "readhomu".to_repr =>
-      #                Repr::set(Repr::getenv, "homu".to_repr,
-      #                          Repr::terminal("homu".to_repr)),
-      #     "printhomu".to_repr => Repr::print(Repr::get(Repr::getenv, "homu".to_repr))
-      #   })
-      # )
 
       root['foreach'] = Repr::function(
         root,
@@ -199,7 +190,6 @@ module Garbanzo
         Repr::begin(
           { "init_i" => Repr::set(Repr::getenv, "i".to_repr, 0.to_repr),
             "init_store" => Repr::set(Repr::getenv, "store".to_repr, Repr::datastore({}.to_repr)),
-#            "debug" => Repr::print("debug".to_repr),
             "loop" => Repr::while(
               Repr::notequal(Repr::get(Repr::getenv, "i".to_repr),
                              Repr::length(Repr::get(Repr::getenv, "string".to_repr))),
@@ -211,101 +201,17 @@ module Garbanzo
                                                              }.to_repr)),
                   "increment" => Repr::set(Repr::getenv, "i".to_repr, Repr::add(Repr::get(Repr::getenv, "i".to_repr), 1.to_repr))
                 }.to_repr)),
-#            "print" => Repr::print(Repr::get(Repr::getenv, "store".to_repr)),
             "choice" => Repr::datastore(
               { "@" => "choice",
                 "children" => Repr::get(Repr::getenv, "store".to_repr)
               }.to_repr)
           }.to_repr))
 
-      
-      # parser['sentence']['children']['hoge'] = Repr::scope(
-      #   { "readhoge" => Repr::terminal('hoge'.to_repr),
-      #     "foreach"  => Repr::call(root['foreach'],
-      #                              { "store" => {
-      #                                  1 => "mado",
-      #                                  2 => "homu",
-      #                                  3 => "saya"
-      #                                }.to_repr,
-      #                                "func" => Repr::function(
-      #                                  Repr::getenv,
-      #                                  Repr::begin(
-      #                                    { "printkey" => Repr::print(Repr::get(Repr::getenv, "key".to_repr)),
-      #                                      "printval" => Repr::print(Repr::get(Repr::getenv, "value".to_repr))
-      #                                    }.to_repr))
-      #                              }.to_repr)
-      #   }.to_repr)
-      
-      # parser['sentence']['children']['number'] = Repr::begin(
-      #   { "head" => set(getenv, "head".to_repr, call(get(get(getenv, "/".to_repr), "oneof".to_repr),
-      #                                                datastore({ "string" => "0123456789" }.to_repr))),
-      #     "tail" => set(getenv, "many(call(get(get(getenv, "/".to_repr), "oneof".to_repr),
-      #                         datastore({ "string" => "0123456789" }.to_repr))),
-      #     "init" => set(getenv, "i".to_repr, 0.to_repr),
-      #     "loop" => Repr::while(
-      #         notequal(get(getenv, "i".to_repr),
-      #                  size(getenv, "string")),
-      #         Repr::begin(
-      #           { "settostore" =>  set(get(getenv, "store".to_repr), get(getenv, "i".to_repr), 
-      #                                  charat(get(getenv, "string"), get(getenv, "i".to_repr))),
-      #             "increment" => set(getenv, "i".to_repr, add(get(getenv, "i".to_repr), 1.to_repr))
-      #           }.to_repr)),
-
-      # parser['string'] = Repr::scope(
-      #   { "beginstring" => Repr::terminal('"'.to_repr),
-      #     "contents"    => Repr::set(Repr::getenv, "tmp".to_repr,
-      #                                Repr::many(Repr::call(root['oneof'], { "string" => " '\n.$@/abcdefghijklmnopqrstuvwxyz" }.to_repr))),
-      #     "endstring"   => Repr::terminal('"'.to_repr),
-      #     "result"      => Repr::set(Repr::getenv, "res".to_repr, "".to_repr),
-      #     "convert"     => Repr::call(root['foreach'],
-      #                                 Repr::datastore(
-      #                                   { "store" => Repr::get(Repr::getenv, "tmp".to_repr),
-      #                                     "func"  => Repr::lambda(
-      #                                       Repr::getenv,
-      #                                       Repr::set(Repr::get(Repr::getenv, "..".to_repr),
-      #                                                 "res".to_repr,
-      #                                                 Repr::append(
-      #                                                   Repr::get(Repr::get(Repr::getenv, "..".to_repr),
-      #                                                             "res".to_repr),
-      #                                                   Repr::get(Repr::getenv, "value".to_repr))))
-      #                                   }.to_repr)),
-      #     "return"      => Repr::get(Repr::getenv, "res".to_repr)
-      #   }.to_repr)
 
       install_string_rule(root)
       install_whitespaces_rule(root)
       install_pair_rule(root)
       install_datastore_rule(root)
-
-      
-      ## root
-      # parser['datastore'] = Repr::scope(
-      #   { "begindatastore" => Repr::terminal("{".to_repr),
-      #     "initresult" => Repr::set(Repr::getenv, "result".to_repr, Repr::datastore({})),
-      #     "readrec" => Repr::many(
-      #       Repr::quote(
-      #         Repr::begin(
-      #           { "readstring"     => Repr::set(
-      #               Repr::getenv, "key".to_repr,
-      #               Repr::eval(Repr::getenv,
-      #                          Repr::get(Repr::get(Repr::get(Repr::getenv, "/".to_repr),
-      #                                              "parser".to_repr),
-      #                                    "expression".to_repr))),
-      #             "separator"      => Repr::terminal(":".to_repr),
-      #             "readexpression" => Repr::set(
-      #               Repr::getenv, "value".to_repr,
-      #               Repr::eval(Repr::getenv,
-      #                          Repr::get(Repr::get(Repr::get(Repr::getenv, "/".to_repr),
-      #                                              "parser".to_repr),
-      #                                    "expression".to_repr))),
-      #             "readcomma" => Repr::terminal(",".to_repr),
-      #             "updatevalue" => Repr::set(Repr::get(Repr::getenv, "result".to_repr),
-      #                                        Repr::get(Repr::getenv, "key".to_repr),
-      #                                        Repr::get(Repr::getenv, "value".to_repr)),
-      #           }.to_repr))),
-      #     "enddatastore" => Repr::terminal("}".to_repr),
-      #     "return" => Repr::get(Repr::getenv, "result".to_repr)
-      #   }.to_repr)
 
 
       ## an expression is either a datastore or a string
@@ -314,7 +220,7 @@ module Garbanzo
           "string" => parser['string']
         }.to_repr)
 
-#      parser['sentence']['children']['expression'] = parser['expression']
+      ## sentence ::= datastore
       parser['sentence']['children']['datastore'] = parser['datastore']
       root
     end

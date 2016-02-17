8627d23061dd3c1ff4686f506a390014d0680add
Akama Hitoshi <akamahitoshi@gmail.com>
2016-02-17 10:24:46 +0900

    add testcase for analyze cache misuse

--- test_evaluator.rb
+++ test_evaluator.rb
@@ -1,3 +1,4 @@
+# coding: utf-8
 require 'test/unit'
 require './evaluator.rb'
 
@@ -17,5 +18,19 @@ class TC_Evaluator < Test::Unit::TestCase
     assert_raise {
       num.analyze
     }
+  end
+
+  def test_change_comname
+    one = 1.to_repr
+    two = 2.to_repr    
+    add = Repr::add(one, two)
+    
+    ev = Evaluator.new
+
+    assert_equal(3.to_repr, ev.evaluate(add)) # まあいいでしょう．
+    add['@'] = "sub".to_repr
+
+    assert_equal(-1.to_repr, ev.evaluate(add)) # analyzeのキャッシュが正しく破棄されていないと，この部分がおかしくなるはず
+    
   end
 end

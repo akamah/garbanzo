27275330733a0584379457c9a283299d33f0905a
Akama Hitoshi <akamahitoshi@gmail.com>
2016-03-04 11:42:07 +0900

    make parsing of version 1 faster

--- parser.rb
+++ parser.rb
@@ -22,40 +22,53 @@ module Garbanzo
       self.parse_rule(grammar.start, source)
     end
 
+    def parse_sequence(rule, source)
+      es, rest = rule.children.reduce([[], source]) do |accum, c|
+        e1, r1 = parse_rule(c, accum[1])
+        [accum[0] << e1, r1]
+      end
+
+      es = rule.func.call(*es) if rule.func != nil
+      return es, rest
+    end
+    
+    def parse_choice(rule, source)
+#      errs = []
+      for c in rule.children
+        begin
+          return parse_rule(c, source)
+        rescue ParseError => e
+#          errs << e
+        end
+      end
+
+      raise ParseError, "no choice "
+#      raise errs.max_by {|a| a.line}
+    end
+
+    def parse_string(rule, source)
+      if source.start_with?(rule.string)
+        return Repr::String.new(rule.string), source[rule.string.length .. -1]
+      else
+        raise ParseError.new("#{rule.message}, around #{source[0..20]}", (10000 - source.length))
+      end
+    end
+    
     def parse_rule(rule, source)
       case rule
-      when Sequence
-        es, rest = rule.children.reduce([[], source]) do |accum, c|
-          e1, r1 = parse_rule(c, accum[1])
-          [accum[0] << e1, r1]
-        end
-
-        es = rule.func.call(*es) if rule.func != nil
-        return es, rest
       when Choice
-        errs = []
-        for c in rule.children
-          begin
-            return parse_rule(c, source)
-          rescue ParseError => e
-            errs << e.message
-          end
-        end
-
-        raise ParseError, errs[0]
+        parse_choice(rule, source)
       when String
-        if source.start_with?(rule.string)
-          return Repr::String.new(rule.string), source[rule.string.length .. -1]
-        else
-          raise ParseError, "#{rule.message} at #{source[0..10]}"
-        end
+        parse_string(rule, source)
+      when Sequence
+        parse_sequence(rule, source)
       when Function
         rule.function.call(source)
       when Call
         if r = grammar.rules[rule.rule_name]
           parse_rule(r, source)
         else
-          raise "rule: #{rule.rule_name}"
+          raise ParseError.new("rule: #{rule.rule_name}", (10000 - source.length))
         end
       when Bind
         x, rest = parse_rule(rule.rule, source)
@@ -69,7 +82,7 @@ module Garbanzo
         rescue ParseError
           return [Repr::store({}), source]
         end
-        raise ParseError, "not #{rule.message}"
+        raise ParseError.new("not #{rule.message}", (10000 - source.length))
       else
         raise "PARSE_RULE: error, not a rule #{rule}"
       end      
